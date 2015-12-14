857cf607d10750d67e6e18d2185600b6d166d0f3
Jean-Pierre Damen <jpdamen@mac.com>
2015-12-14 23:19:05 +0100

    Test without view

--- src/main/groovy/com/neoteric/jenkins/JenkinsApi.groovy
+++ src/main/groovy/com/neoteric/jenkins/JenkinsApi.groovy
@@ -56,13 +56,11 @@ class JenkinsApi {
 	}
 
 	void cloneJobForBranch(String jobPrefix, ConcreteJob missingJob, String createJobInView, String gitUrl) {
-		String createJobInViewPath = resolveViewPath(createJobInView)
-		println "-----> createInView after" + createJobInView
 		String missingJobConfig = configForMissingJob(missingJob, gitUrl)
 		TemplateJob templateJob = missingJob.templateJob
 
 		//Copy job with jenkins copy job api, this will make sure jenkins plugins get the call to make a copy if needed (promoted builds plugin needs this)
-		post(createJobInViewPath + 'createItem', missingJobConfig, [name: missingJob.jobName, mode: 'copy', from: templateJob.jobName], ContentType.XML)
+		post('createItem', missingJobConfig, [name: missingJob.jobName, mode: 'copy', from: templateJob.jobName], ContentType.XML)
 
 		post('job/' + missingJob.jobName + "/config.xml", missingJobConfig, [:], ContentType.XML)
 		//Forced disable enable to work around Jenkins' automatic disabling of clones jobs
