2710620f483f88dbb7f67b672eacac295d6b069b
Jean-Pierre Damen <jpdamen@mac.com>
2015-12-15 01:05:27 +0100

    TEST

--- src/main/groovy/com/neoteric/jenkins/JenkinsJobManager.groovy
+++ src/main/groovy/com/neoteric/jenkins/JenkinsJobManager.groovy
@@ -102,11 +102,13 @@ class JenkinsJobManager {
 				expectedJobsPerBranch.each { println "           $it" }
 				List<String> jobNamesPerBranch = jobNames.findAll{ it.endsWith(branchToProcess) }
 				println "-------> Job Names per branch:"
+				println jobNamesPerBranch
 				jobNamesPerBranch.each { println "           $it" }
 				List<ConcreteJob> missingJobsPerBranch = expectedJobsPerBranch.findAll { expectedJob ->
 					!jobNamesPerBranch.any {it.contains(expectedJob.jobName) }
 				}
 				println "-------> Missing jobs:"
+				println missingJobsPerBranch
 				missingJobsPerBranch.each { println "           $it" }
 				missingJobs.addAll(missingJobsPerBranch)
 			}
